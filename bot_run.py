7403978ccb0dd94a6cfa50c21d895803130cb7a2
nichnikov <a.nichnikov@gmail.com>
2023-07-17 22:24:47 +0300

    add function

--- bot_run.py
+++ bot_run.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot('5905049745:AAGJSK1_eJVYsMfhM_aCHZ7p9_1Zrn7hRhA')
+bot = telebot.TeleBot('MY-TOKEN')
 
 @bot.message_handler(commands = ['start'])
 def url(message):
@@ -9,3 +9,14 @@ def url(message):
     btn1 = types.InlineKeyboardButton(text='Наш сайт', url='https://habr.com/ru/all/')
     markup.add(btn1)
     bot.send_message(message.from_user.id, "По кнопке ниже можно перейти на сайт хабра", reply_markup = markup)
+
+@bot.message_handler(commands=['languages'])
+def start(message):
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("🇷🇺 Русский")
+    btn2 = types.KeyboardButton('🇬🇧 English')
+    markup.add(btn1, btn2)
+    bot.send_message(message.from_user.id, "🇷🇺 Выберите язык / 🇬🇧 Choose your language", reply_markup=markup)
+
+bot.polling(none_stop=True, interval=0) 
